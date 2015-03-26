b1a2d41fcc19af40bbbc38157493707b3beed462
Jeren Hicks <jeren.hicks@leidos.com>
2015-03-26 12:08:40 -0500

    Adding another controller for testing purposes.

---
+++ src/com/ocean/template/jersey/controllers/Test2Controller.java
@@ -0,0 +1,45 @@
+package com.ocean.template.jersey.controllers;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.ocean.template.jersey.model.Test;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
+/**
+ * Test controller that will handle demonstrating the api functionality.
+ * @author arcticrain
+ *
+ */
+@Path("/test2")
+@Api(value = "/test2", description = "This is an example of another controller.")
+public class Test2Controller {
+	/** random value.*/
+	private Random random = new Random();
+		
+	/**
+	 * Test method that will handle returning a test class back. 
+	 * @return a random test class.
+	 */
+	@GET
+	@Produces("application/json")
+	@ApiOperation(value = "Retrieves a random Test objects", notes = "Build to test out functionality.", response = Test.class, responseContainer = "Test")
+	public Test getTest() {
+		Test test = new Test(new BigInteger(130, random).toString(32), new BigInteger(130, random).toString(32));
+	    return test;
+	}
+	
+	@POST
+	@Produces("application/json")
+	@ApiOperation(value = "Adds the test and returns it.", notes = "More test notes here.", response = Test.class, responseContainer = "Test")
+	public Test addTest(String one, String two) {
+		return new Test(one, two);
+	}
+	
+}
